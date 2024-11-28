fcdb712f50d8e1c77a97ec02914d15366e333e00
Rajan <acharya.rajan.raj11@gmail.com>
2024-11-28 18:52:40 +0545

    conditional rendering for culture form

--- frontend/src/components/add-culture-form/CultureForm.jsx
+++ frontend/src/components/add-culture-form/CultureForm.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const CultureForm = () => {
   const [loading, setLoading] = useState(false);
@@ -10,6 +13,8 @@ const CultureForm = () => {
     content: "",
   });
   const [images, setImages] = useState([]);
+
+  const authStatus = useSelector((state) => state.auth.status);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -76,99 +81,111 @@ const CultureForm = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h2 className="text-2xl font-semibold mb-4">Create New Post</h2>
-
-      <form onSubmit={submitPost} className="space-y-4">
-        {/* Title Input */}
-        <div className="flex flex-col">
-          <label htmlFor="title" className="text-sm font-medium">
-            Post Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            value={postData.title}
-            onChange={handleChange}
-            className="border p-2 rounded"
-            placeholder="Enter post title"
-            required
-          />
-        </div>
-        {/* Category Dropdown */}
-        <div className="flex flex-col">
-          <label htmlFor="category" className="text-sm font-medium">
-            Category
-          </label>
-          <select
-            name="category"
-            id="category"
-            value={postData.category || ""}
-            onChange={handleChange}
-            className="border p-2 rounded"
-            required
-          >
-            <option value="" disabled>
-              Select a category
-            </option>
-            <option value="festival">Festival</option>
-            <option value="dance">Dance</option>
-            <option value="food">Food</option>
-            <option value="art and music">Art and Music</option>
-            <option value="language">Language</option>
-            <option value="other">Language</option>
-          </select>
-        </div>
-
-        {/* Content Input */}
-        <div className="flex flex-col">
-          <label htmlFor="content" className="text-sm font-medium">
-            Post Content
-          </label>
-          <textarea
-            name="content"
-            id="content"
-            value={postData.content}
-            onChange={handleChange}
-            className="border p-2 rounded"
-            placeholder="Enter post content"
-            rows="5"
-            required
-          ></textarea>
-        </div>
-
-        {/* Image Upload */}
-        <div className="flex flex-col">
-          <label htmlFor="image-upload" className="text-sm font-medium">
-            Featured Images
-          </label>
-          <input
-            type="file"
-            id="image-upload"
-            multiple
-            onChange={handleFileChange}
-            className="border p-2 rounded"
-            accept="image/png, image/jpg, image/jpeg, image/gif"
-          />
-        </div>
-
-        {/* Submit Button */}
-        <div className="flex justify-center">
-          <button
-            type="submit"
-            className="bg-blue-500 text-white py-2 px-6 rounded"
-            disabled={loading}
-          >
-            {loading ? "Submitting..." : "Submit Post"}
-          </button>
-        </div>
-      </form>
-
-      {/* Display Success or Error Message */}
-      {successMessage && (
-        <p className="text-green-500 mt-4">{successMessage}</p>
+      {!authStatus && (
+        <h1>
+          Login to Share The culture.{" "}
+          <Link to="/login" className="text-blue-600">
+            Login Now
+          </Link>
+        </h1>
       )}
-      {errorMessage && <p className="text-red-500 mt-4">{errorMessage}</p>}
+
+      {authStatus && (
+        <span>
+          <h2 className="text-2xl font-semibold mb-4">Create New Post</h2>
+          <form onSubmit={submitPost} className="space-y-4">
+            {/* Title Input */}
+            <div className="flex flex-col">
+              <label htmlFor="title" className="text-sm font-medium">
+                Post Title
+              </label>
+              <input
+                type="text"
+                name="title"
+                id="title"
+                value={postData.title}
+                onChange={handleChange}
+                className="border p-2 rounded"
+                placeholder="Enter post title"
+                required
+              />
+            </div>
+            {/* Category Dropdown */}
+            <div className="flex flex-col">
+              <label htmlFor="category" className="text-sm font-medium">
+                Category
+              </label>
+              <select
+                name="category"
+                id="category"
+                value={postData.category || ""}
+                onChange={handleChange}
+                className="border p-2 rounded"
+                required
+              >
+                <option value="" disabled>
+                  Select a category
+                </option>
+                <option value="festival">Festival</option>
+                <option value="dance">Dance</option>
+                <option value="food">Food</option>
+                <option value="art and music">Art and Music</option>
+                <option value="language">Language</option>
+                <option value="other">Language</option>
+              </select>
+            </div>
+
+            {/* Content Input */}
+            <div className="flex flex-col">
+              <label htmlFor="content" className="text-sm font-medium">
+                Post Content
+              </label>
+              <textarea
+                name="content"
+                id="content"
+                value={postData.content}
+                onChange={handleChange}
+                className="border p-2 rounded"
+                placeholder="Enter post content"
+                rows="5"
+                required
+              ></textarea>
+            </div>
+
+            {/* Image Upload */}
+            <div className="flex flex-col">
+              <label htmlFor="image-upload" className="text-sm font-medium">
+                Featured Images
+              </label>
+              <input
+                type="file"
+                id="image-upload"
+                multiple
+                onChange={handleFileChange}
+                className="border p-2 rounded"
+                accept="image/png, image/jpg, image/jpeg, image/gif"
+              />
+            </div>
+
+            {/* Submit Button */}
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                className="bg-blue-500 text-white py-2 px-6 rounded"
+                disabled={loading}
+              >
+                {loading ? "Submitting..." : "Submit Post"}
+              </button>
+            </div>
+          </form>
+
+          {/* Display Success or Error Message */}
+          {successMessage && (
+            <p className="text-green-500 mt-4">{successMessage}</p>
+          )}
+          {errorMessage && <p className="text-red-500 mt-4">{errorMessage}</p>}
+        </span>
+      )}
     </div>
   );
 };
