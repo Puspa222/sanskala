896462d84718c6162bb114e18030dad27645ed19
Rajan <acharya.rajan.raj11@gmail.com>
2024-11-28 11:12:13 +0545

    App.jsx routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import "./App.css";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-    <Navbar />
-
-    </>
+    <div className="flex">
+      <Navbar />
+      <div className="flex-1 p-4 bg-gray-100 dark:bg-gray-900">
+        <Outlet />
+      </div>
+    </div>
   );
 }
 export default App;
